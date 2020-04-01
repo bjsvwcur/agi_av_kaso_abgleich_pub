b1cad2c59ff373854f1088d118f67bbfb9d4615a
bjsvwcur <sandra.curiger@bd.so.ch>
2020-04-01 15:05:01 +0200

    new test kaso abgleich

---
+++ development_dbs/kaso_abgleich.sql
@@ -0,0 +1,89 @@
+SET ROLE sogis_admin
+;
+
+CREATE SCHEMA agi_av_kaso_abgleich_import
+;
+
+GRANT USAGE ON SCHEMA agi_av_kaso_abgleich_import TO admin, gretl
+;
+COMMENT ON SCHEMA agi_av_kaso_abgleich_import
+  IS 'exportierte Daten von KASO'
+;
+
+CREATE TABLE agi_av_kaso_abgleich_import.kaso_daten (
+    t_id serial,
+    geb_gdeid int4 NULL,
+    geb_bfsnr int4 NULL,
+    geb_gemname varchar NULL,
+    gb_nr varchar NULL,
+    gb_flaeche float4 NULL,
+    gb_art int4 NULL,
+    gb_gueltigbis varchar NULL,
+    PRIMARY KEY (t_id)
+) 
+;
+COMMENT ON TABLE agi_av_kaso_abgleich_import.kaso_daten IS 'Tabelle in welche die Daten von der KASO importiert werden' 
+;
+GRANT SELECT,UPDATE,DELETE,INSERT ON TABLE agi_av_kaso_abgleich_import.kaso_daten TO admin, gretl
+;
+GRANT USAGE ON SEQUENCE agi_av_kaso_abgleich_import.kaso_daten_t_id_seq TO admin, gretl
+;
+
+CREATE TABLE agi_av_kaso_abgleich_import.differenzen_staging (
+    t_id serial NOT NULL,
+    av_lieferdatum date NULL,
+    av_gem_bfs int4 NULL,
+    av_nbident varchar NULL,
+    av_nummer text NULL,
+    av_art int4 NULL,
+    av_art_txt varchar NULL,
+    av_flaeche float8 NULL,
+    av_mutation_id varchar NULL,
+    av_mut_beschreibung varchar NULL,
+    kaso_gemeinde varchar NULL,
+    kaso_bfs_nr int4 NULL,
+    kaso_nbident varchar NULL,
+    kaso_gb_nr varchar NULL,
+    kaso_art int4 NULL,
+    kaso_flaeche float4 NULL,
+    flaechen_differenz float8 NULL,
+    fehlerart int4 NULL,
+    fehlerart_text varchar NULL,
+    PRIMARY KEY (t_id)
+)
+;
+COMMENT ON COLUMN agi_av_kaso_abgleich_import.differenzen_staging.fehlerart IS '1 : ''Liegenschaft mit falscher Art in KASO'', 
+2 : ''Grundstücke mit Flächendifferenzen'', 
+3 : ''Grundstück kommt nur in den AV-Daten vor'', 
+4 : ''Grundstück kommt nur in den KASO-Daten vor''' ; 
+;
+GRANT SELECT,UPDATE,DELETE,INSERT ON TABLE agi_av_kaso_abgleich_import.differenzen_staging TO admin,gretl
+;
+
+COMMENT ON TABLE agi_av_kaso_abgleich_import.differenzen_staging
+  IS 'Differenzen zwischen den AV-Daten und den Katasterschätzungsdaten'
+;
+SELECT AddGeometryColumn('agi_av_kaso_abgleich_import', 'differenzen_staging', 'geometrie', 2056, 'POLYGON', 2)
+;
+GRANT USAGE ON SEQUENCE agi_av_kaso_abgleich_import.differenzen_staging_t_id_seq TO admin,gretl
+;
+
+
+CREATE TABLE agi_av_kaso_abgleich_import.uebersicht_des_vergleichs_staging (
+    t_id serial NOT NULL,
+    gem_bfs int4 NULL,
+    name varchar NULL,
+    anzahl_differenzen int8 NULL,
+    PRIMARY KEY (t_id)
+)
+;
+GRANT SELECT,UPDATE,DELETE,INSERT ON TABLE agi_av_kaso_abgleich_import.uebersicht_des_vergleichs_staging TO admin,gretl
+;
+
+COMMENT ON TABLE agi_av_kaso_abgleich_import.uebersicht_des_vergleichs_staging
+  IS 'Anzahl Differenzen zwischen den AV-Daten und den Katasterschätzungsdaten pro Gemeinde'
+;
+SELECT AddGeometryColumn('agi_av_kaso_abgleich_import', 'uebersicht_des_vergleichs_staging', 'geometrie', 2056, 'MULTIPOLYGON', 2)
+;
+GRANT USAGE ON SEQUENCE agi_av_kaso_abgleich_import.uebersicht_des_vergleichs_staging_t_id_seq TO admin,gretl
+;
---
+++ development_dbs/kaso_abgleich_pub.sql
@@ -0,0 +1,72 @@
+CREATE ROLE agi_av_kaso_abgleich_pub_read
+;
+
+CREATE SCHEMA agi_av_kaso_abgleich_pub
+;
+
+GRANT USAGE ON SCHEMA agi_av_kaso_abgleich_pub TO admin,gretl
+;
+
+COMMENT ON SCHEMA agi_av_kaso_abgleich_pub IS 'Vergleich der AV-Daten mit den Katasterschätzungsdaten. Fragen: AGI'
+;
+
+CREATE TABLE agi_av_kaso_abgleich_pub.differenzen (
+    t_id int4 NOT NULL,
+    geometrie geometry(Polygon, 2056) NULL,
+    av_lieferdatum date NULL,
+    av_gem_bfs int4 NULL,
+    av_nbident varchar NULL,
+    av_nummer text NULL,
+    av_art int4 NULL,
+    av_art_txt varchar NULL,
+    av_flaeche float8 NULL,
+    av_mutation_id varchar NULL,
+    av_mut_beschreibung varchar NULL,
+    kaso_gemeinde varchar NULL,
+    kaso_bfs_nr int4 NULL,
+    kaso_nbident varchar NULL,
+    kaso_gb_nr varchar NULL,
+    kaso_art int4 NULL,
+    kaso_flaeche float4 NULL,
+    flaechen_differenz float8 NULL,
+    fehlerart int4 NULL,
+    PRIMARY KEY (t_id)
+)
+WITH (
+    OIDS=FALSE
+) 
+;
+COMMENT ON COLUMN agi_av_kaso_abgleich_pub.differenzen.fehlerart IS '1 : ''Liegenschaft mit falscher Art in KASO'', 
+2 : ''Grundstücke mit Flächendifferenzen'', 
+3 : ''Grundstück kommt nur in den AV-Daten vor'', 
+4 : ''Grundstück kommt nur in den KASO-Daten vor''' ; 
+;
+GRANT SELECT ON TABLE agi_av_kaso_abgleich_pub.differenzen TO admin,gretl
+;
+GRANT SELECT,UPDATE,DELETE,INSERT ON TABLE agi_av_kaso_abgleich_pub.differenzen TO admin,gretl
+;
+
+COMMENT ON TABLE agi_av_kaso_abgleich_pub.differenzen
+  IS 'Differenzen zwischen den AV-Daten und den Katasterschätzungsdaten'
+;
+
+CREATE TABLE agi_av_kaso_abgleich_pub.uebersicht_des_vergleichs (
+    t_id serial NOT NULL,
+    geometrie geometry(Polygon, 2056)NOT NULL,
+    gem_bfs int4 NULL,
+    name varchar NULL,
+    anzahl_differenzen int8 NULL,
+    PRIMARY KEY (t_id)
+)
+WITH (
+    OIDS = FALSE
+)
+;
+GRANT SELECT ON TABLE agi_av_kaso_abgleich_pub.uebersicht_des_vergleichs TO admin,gretl
+;
+GRANT SELECT,UPDATE,DELETE,INSERT ON TABLE agi_av_kaso_abgleich_pub.uebersicht_des_vergleichs TO admin,gretl
+;
+
+COMMENT ON TABLE agi_av_kaso_abgleich_pub.uebersicht_des_vergleichs
+  IS 'Anzahl Differenzen zwischen den AV-Daten und den Katasterschätzungsdaten pro Gemeinde'
+;
